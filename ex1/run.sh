f4c092dd958b551678abf5a6cd003060ad0901f1
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-05-24 18:55:54 +0900

    Add description about starting at different time

--- ex1/run.sh
+++ ex1/run.sh
@@ -61,3 +61,64 @@ set multiplot;
 plot
   "bandwidth_same_start.dat" u 1:($2+$3) title "tcp2" with filledcurves x1,
   "bandwidth_same_start.dat" u 1:($2) title "tcp1" with filledcurves x1;'
+
+# different start
+ns different_start.tcl
+
+grep "0  0  4  0  cwnd" different_start.tcp > cwnd_tcp0_different_start.dat
+grep "1  0  5  0  cwnd" different_start.tcp > cwnd_tcp1_different_start.dat
+
+gnuplot -e "
+set terminal png;
+set output 'cwnd_different_start.png';
+set xrange [0:100];
+set yrange [0:80];
+plot 'cwnd_tcp0_different_start.dat' u 1:7 w l, 'cwnd_tcp1_different_start.dat' u 1:7 w l;"
+
+
+awk '$1 == "r" && $3 == "2" && $4 == "3" && $8 == "1"' \
+    < different_start.tr \
+    | perl -e '
+$time = 0;
+$bw = 0;
+while (<>) {
+  @field = split(/ /);
+  if ($field[1] > ($time + 1)) {
+    print "$time $bw\n";
+    $time += 1;
+    $bw = 8 * $field[5];
+  } else {
+    $bw += 8 * $field[5];
+  }
+}
+print "$time $bw\n";' \
+    > bandwidth_tcp0_different_start.dat
+
+awk '$1 == "r" && $3 == "2" && $4 == "3" && $8 == "2"' \
+    < different_start.tr \
+    | perl -e '
+$time = 0;
+$bw = 0;
+while (<>) {
+  @field = split(/ /);
+  if ($field[1] > ($time + 1)) {
+    print "$time $bw\n";
+    $time += 1;
+    $bw = 8 * $field[5];
+  } else {
+    $bw += 8 * $field[5];
+  }
+}
+print "$time $bw\n";' \
+    > bandwidth_tcp1_different_start.dat
+
+join bandwidth_tcp0_different_start.dat bandwidth_tcp1_different_start.dat \
+    > bandwidth_different_start.dat
+
+gnuplot -e '
+set terminal png;
+set output "bandwidth_different_start.png";
+set multiplot;
+plot
+  "bandwidth_different_start.dat" u 1:($2+$3) title "tcp2" with filledcurves x1,
+  "bandwidth_different_start.dat" u 1:($2) title "tcp1" with filledcurves x1;'
