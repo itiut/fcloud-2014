fcd31e7efbfe6471c1bbe55a1539e23a6a64d3e4
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-05-24 18:31:48 +0900

    Change output image file name
    same_start.png      ->
    cwnd_same_start.png
    ^^^^^

--- ex1/run.sh
+++ ex1/run.sh
@@ -9,7 +9,7 @@ grep "1  0  5  0  cwnd" same_start.tcp > cwnd_tcp1_same_start.dat
 
 gnuplot -e "
 set terminal png;
-set output 'same_start.png';
+set output 'cwnd_same_start.png';
 set xrange [0:100];
 set yrange [0:80];
 plot 'cwnd_tcp0_same_start.dat' u 1:7 w l, 'cwnd_tcp1_same_start.dat' u 1:7 w l;"
