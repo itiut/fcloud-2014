e1ace6a2c380e4dfa5e33e7fae142293e855947f
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-05-24 20:31:11 +0900

    Remove range specification

--- ex1/run.sh
+++ ex1/run.sh
@@ -10,8 +10,6 @@ grep "1  0  5  0  cwnd" same_start.tcp > cwnd_tcp1_same_start.dat
 gnuplot -e "
 set terminal png;
 set output 'cwnd_same_start.png';
-set xrange [0:100];
-set yrange [0:80];
 plot
   'cwnd_tcp0_same_start.dat' u 1:7 w l title 'tcp0',
   'cwnd_tcp1_same_start.dat' u 1:7 w l title 'tcp1';"
@@ -73,8 +71,6 @@ grep "1  0  5  0  cwnd" different_start.tcp > cwnd_tcp1_different_start.dat
 gnuplot -e "
 set terminal png;
 set output 'cwnd_different_start.png';
-set xrange [0:100];
-set yrange [0:80];
 plot
   'cwnd_tcp0_different_start.dat' u 1:7 w l title 'tcp0',
   'cwnd_tcp1_different_start.dat' u 1:7 w l title 'tcp1';"
